9a331915696f52507caea2a151589cd15d62705f
parthz <parthsaradhi1992@gmail.com>
2019-01-12 14:45:08 +0530

    iterators

---
+++ iterators.rb
@@ -0,0 +1,29 @@
+=begin
+Understanding iterators
+=end
+
+products = ["Alexa Device", "RedMi Pro", "MacBook Pro", "Magic Mouse"]
+
+products.each do |product|
+    puts "#{product}"
+end
+
+employess = {
+    "name" => "Partha",
+    "designation" => "Senior Software Engineer",
+    "company" => {
+        "name" => "Inmar, Inc",
+        "location" => "Hyderabad"
+    }
+}
+
+employess.each do |key, value|
+    puts "#{key} => #{value}"
+end
+
+new_employees = employess.collect{
+    |x|
+    x
+}
+
+puts "#{new_employees}"
