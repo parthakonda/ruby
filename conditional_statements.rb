f3e371d0a5d843f6aff72b85f847ba887b4f28d4
parthz <parthsaradhi1992@gmail.com>
2019-01-12 16:04:47 +0530

    conditional_statements

---
+++ conditional_statements.rb
@@ -0,0 +1,38 @@
+=begin
+Understanding conditional statement such as
+if..else, case, unless
+=end
+
+marks = 22
+
+if marks < 18 and marks > 0
+    puts "failed"
+elsif marks > 17 and marks < 35
+    puts "passed"
+elsif marks > 34 and marks < 75
+    puts "first class"
+elsif marks > 74 and marks <= 100
+    puts "Distinction"
+else
+    puts "Invalid marks"
+end
+
+unless marks > 0 and marks < 100
+    puts "Invalid marks"
+end
+
+y = 10 if marks < 23
+puts y if defined? y
+
+case marks
+when 0..18
+    puts "failed"
+when 19..35
+    puts "passed"
+when 36..75
+    puts "first class"
+when 76..100
+    puts "Distinction"
+else
+    puts "Invalid marks"
+end
